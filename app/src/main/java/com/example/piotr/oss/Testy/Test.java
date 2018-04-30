cdb17a36eadcb46a6882f3cddd52a0af03ef2d0f
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-30 11:56:41 +0200

    Make test layout common

--- app/src/main/java/com/example/piotr/oss/Testy/Test.java
+++ app/src/main/java/com/example/piotr/oss/Testy/Test.java
@@ -1,6 +1,7 @@
 package com.example.piotr.oss.Testy;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,7 @@ public class Test extends AppCompatActivity {
 
     SeekBar seekBar;
     TextView txtMode;
-    Button btnPlay,btnScore;
+    Button btnPlay, btnScore;
     DbHelper db;
     ImageView Back;
 
@@ -28,34 +29,34 @@ public class Test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        ImageView background = findViewById(R.id.lungs);
+        Drawable image = getResources().getDrawable(R.drawable.lungs);
+        background.setImageDrawable(image);
 
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
-        txtMode = (TextView)findViewById(R.id.txtMode);
-        btnPlay = (Button)findViewById(R.id.btnPlay);
-        btnScore = (Button)findViewById(R.id.btnScore);
-        Back = (ImageView)findViewById(R.id.Back);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        txtMode = (TextView) findViewById(R.id.txtMode);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnScore = (Button) findViewById(R.id.btnScore);
+        Back = (ImageView) findViewById(R.id.Back);
 
 
         db = new DbHelper(this);
-        try{
+        try {
             db.createDataBase();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
 
 
         //Event
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(progress == 0)
+                if (progress == 0)
                     txtMode.setText("5 pytań".toString());
-                else if(progress == 1)
+                else if (progress == 1)
                     txtMode.setText("15 pytań".toString());
-                else if(progress == 2)
+                else if (progress == 2)
                     txtMode.setText("25 pytań".toString());
             }
 
@@ -72,8 +73,8 @@ public class Test extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Playing.class);
-                intent.putExtra("MODE",getPlayMode()); // Send Mode to Playing page
+                Intent intent = new Intent(getApplicationContext(), Playing.class);
+                intent.putExtra("MODE", getPlayMode()); // Send Mode to Playing page
                 startActivity(intent);
                 finish();
             }
@@ -82,7 +83,7 @@ public class Test extends AppCompatActivity {
         btnScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Score.class);
+                Intent intent = new Intent(getApplicationContext(), Score.class);
                 startActivity(intent);
                 finish();
             }
@@ -91,7 +92,7 @@ public class Test extends AppCompatActivity {
         Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),StrefaTestu.class);
+                Intent intent = new Intent(getApplicationContext(), StrefaTestu.class);
                 startActivity(intent);
                 finish();
             }
@@ -99,11 +100,11 @@ public class Test extends AppCompatActivity {
     }
 
     private String getPlayMode() {
-        if(seekBar.getProgress()==0)
+        if (seekBar.getProgress() == 0)
             return Common.MODE.EASY.toString();
-        else if(seekBar.getProgress()==1)
+        else if (seekBar.getProgress() == 1)
             return Common.MODE.MEDIUM.toString();
-        else if(seekBar.getProgress()==2)
+        else if (seekBar.getProgress() == 2)
             return Common.MODE.HARD.toString();
 
         return null;
--- app/src/main/java/com/example/piotr/oss/Testy/Test1.java
+++ app/src/main/java/com/example/piotr/oss/Testy/Test1.java
@@ -1,6 +1,7 @@
 package com.example.piotr.oss.Testy;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,50 +14,49 @@ import com.example.piotr.oss.Testy.Common.Common;
 import com.example.piotr.oss.Testy.DbHelper.Db1Helper;
 import com.example.piotr.oss.R;
 
-
 import java.io.IOException;
 
 public class Test1 extends AppCompatActivity {
 
     SeekBar seekBar;
     TextView txtMode;
-    Button btnPlay,btnScore;
+    Button btnPlay, btnScore;
     Db1Helper db;
     ImageView Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test1);
+        setContentView(R.layout.activity_test);
 
+        ImageView background = findViewById(R.id.lungs);
+        Drawable image = getResources().getDrawable(R.drawable.heart);
+        background.setImageDrawable(image);
 
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
-        txtMode = (TextView)findViewById(R.id.txtMode);
-        btnPlay = (Button)findViewById(R.id.btnPlay);
-        btnScore = (Button)findViewById(R.id.btnScore);
-        Back = (ImageView)findViewById(R.id.Back);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        txtMode = (TextView) findViewById(R.id.txtMode);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnScore = (Button) findViewById(R.id.btnScore);
+        Back = (ImageView) findViewById(R.id.Back);
 
 
         db = new Db1Helper(this);
-        try{
+        try {
             db.createDataBase();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
 
 
         //Event
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(progress == 0)
+                if (progress == 0)
                     txtMode.setText("5 pytań".toString());
-                else if(progress == 1)
+                else if (progress == 1)
                     txtMode.setText("15 pytań".toString());
-                else if(progress == 2)
+                else if (progress == 2)
                     txtMode.setText("25 pytań".toString());
             }
 
@@ -73,8 +73,8 @@ public class Test1 extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Playing1.class);
-                intent.putExtra("MODE",getPlayMode()); // Send Mode to Playing page
+                Intent intent = new Intent(getApplicationContext(), Playing1.class);
+                intent.putExtra("MODE", getPlayMode()); // Send Mode to Playing page
                 startActivity(intent);
                 finish();
             }
@@ -83,7 +83,7 @@ public class Test1 extends AppCompatActivity {
         btnScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Score1.class);
+                Intent intent = new Intent(getApplicationContext(), Score1.class);
                 startActivity(intent);
                 finish();
             }
@@ -92,7 +92,7 @@ public class Test1 extends AppCompatActivity {
         Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),StrefaTestu.class);
+                Intent intent = new Intent(getApplicationContext(), StrefaTestu.class);
                 startActivity(intent);
                 finish();
             }
@@ -100,11 +100,11 @@ public class Test1 extends AppCompatActivity {
     }
 
     private String getPlayMode() {
-        if(seekBar.getProgress()==0)
+        if (seekBar.getProgress() == 0)
             return Common.MODE.EASY.toString();
-        else if(seekBar.getProgress()==1)
+        else if (seekBar.getProgress() == 1)
             return Common.MODE.MEDIUM.toString();
-        else if(seekBar.getProgress()==2)
+        else if (seekBar.getProgress() == 2)
             return Common.MODE.HARD.toString();
 
         return null;
