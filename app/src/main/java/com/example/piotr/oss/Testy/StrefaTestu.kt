eae2b339b4744ffdebdce76d1cd9af1584f96897
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-02 09:37:31 +0200

    Add proper up button

--- app/src/main/java/com/example/piotr/oss/Testy/StrefaTestu.kt
+++ app/src/main/java/com/example/piotr/oss/Testy/StrefaTestu.kt
@@ -1,17 +1,21 @@
 package com.example.piotr.oss.Testy
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.piotr.oss.MainActivity
 import com.example.piotr.oss.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class StrefaTestu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_strefa_testu)
+        setSupportActionBar(customtoolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
     fun toMainActivity (view: View) {
         val intent = Intent(applicationContext, MainActivity::class.java)
